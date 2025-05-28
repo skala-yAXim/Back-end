d81a17fc32cf4edb82f3bbacba73191548d3c5b7
Kim Se Eun <rlatpdms1014@gmail.com>
2025-05-28 20:17:59 +0900

    feat: Security 및 Swagger 설정

---
+++ yaxim/src/main/java/com/yaxim/YaximApplication.java
@@ -0,0 +1,13 @@
+package com.yaxim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaximApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaximApplication.class, args);
+    }
+
+}
