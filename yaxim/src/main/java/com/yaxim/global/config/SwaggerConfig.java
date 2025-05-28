99ed334c2d5c77bd66de6b3fba5de3ac8f43bcc8
Kim Se Eun <rlatpdms1014@gmail.com>
2025-05-28 20:17:59 +0900

    feat: Security 및 Swagger 설정

---
+++ yaxim/src/main/java/com/yaxim/global/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.yaxim.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Yaxim API")
+                        .version("v1.0.0")
+                        .description("Yaxim 개인 업무 관리 플랫폼 Swagger API 문서입니다.")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
