2c09e23e53bcac3f2f57f18927c513335d55ae1d
Kim Se Eun <rlatpdms1014@gmail.com>
2025-05-28 20:30:41 +0900

    feat: 회원 Entity 및 Repository 추가

---
+++ yaxim/src/main/java/com/yaxim/user/entity/user/UserRole.java
@@ -0,0 +1,57 @@
+package com.yaxim.user.entity.user;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
+public enum UserRole {
+    GUEST("GUEST"),
+    USER("MEMBER"),
+    LEADER("LEADER"),
+    ADMIN(combine("ADMIN", "MEMBER", "LEADER"));
+
+    @JsonValue
+    private final String name;
+
+    UserRole(String name) {
+        this.name = name;
+    }
+
+    public static String combine(String... names) {
+        return String.join(",", names);
+    }
+
+    private static final Map<String, UserRole> BY_LABEL =
+            Stream.of(values()).collect(Collectors.toMap(UserRole::getName, e -> e));
+
+    public static UserRole of(String name) {
+        return BY_LABEL.get(name);
+    }
+
+    public boolean isAdmin() {
+        return this == ADMIN;
+    }
+
+    public boolean isOwner() {
+        return this == LEADER;
+    }
+
+    public boolean isUser() {
+        return this == USER;
+    }
+
+    @JsonCreator
+    public static UserRole from(String val) {
+        for (UserRole role : UserRole.values()) {
+            if (role.name().equals(val)) {
+                return role;
+            }
+        }
+        return null;
+    }
+}
