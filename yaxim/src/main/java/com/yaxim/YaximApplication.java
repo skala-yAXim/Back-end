7c4d516478f27f25841e544209a3e6d24cc7a08e
Kim Se Eun <rlatpdms1014@gmail.com>
2025-05-30 03:06:02 +0900

    feat: 서버 시간을 Asia/Seoul로 설정

--- yaxim/src/main/java/com/yaxim/YaximApplication.java
+++ yaxim/src/main/java/com/yaxim/YaximApplication.java
@@ -3,10 +3,12 @@ package com.yaxim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
 @SpringBootApplication
 public class YaximApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(YaximApplication.class, args);
     }
 
