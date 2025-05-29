f127209ac3afadf423cafd69c2b56e9478961a83
Kim Se Eun <rlatpdms1014@gmail.com>
2025-05-30 03:15:43 +0900

    feat: JWT에 유저 정보 저장 및 토큰 재발급, 로그아웃 로직 추가

--- yaxim/src/main/java/com/yaxim/global/auth/jwt/TokenService.java
+++ yaxim/src/main/java/com/yaxim/global/auth/jwt/TokenService.java
@@ -1,24 +1,47 @@
 package com.yaxim.global.auth.jwt;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TokenService {
+    private final String ACCESS_TOKEN = "access:";
+    private final String REFRESH_TOKEN = "refresh:";
     private final RedisTemplate<String, String> redisTemplate;
 
-    public void storeRefreshTokenJti(String userId, String jti) {
-        redisTemplate.opsForValue().set(userId, jti);
+    public void storeAccessTokenJti(String userId, String jti, Duration ttl) {
+        redisTemplate.opsForValue().set(ACCESS_TOKEN + userId, jti, ttl);
     }
 
-    public void resetRefreshTokenJti(String userId, String token) {
-        redisTemplate.opsForValue().setIfPresent(userId, token);
+    public boolean isAccessTokenInvalid(String userId, String jti) {
+        String storedJti = redisTemplate.opsForValue().get(ACCESS_TOKEN + userId);
+        return !jti.equals(storedJti);
     }
 
-    public boolean isRefreshTokenValid(String userId, String jti) {
-        String storedJti = redisTemplate.opsForValue().get(userId);
-        return jti.equals(storedJti);
+    public void invalidateAccessToken(String userId) {
+        redisTemplate.delete(ACCESS_TOKEN + userId);
+    }
+
+    public void storeRefreshTokenJti(String userId, String jti, Duration ttl) {
+        redisTemplate.opsForValue().set(REFRESH_TOKEN + userId, jti, ttl);
+    }
+
+    public boolean isRefreshTokenInvalid(String userId, String jti) {
+        String storedJti = redisTemplate.opsForValue().get(REFRESH_TOKEN + userId);
+        return !jti.equals(storedJti);
+    }
+
+    public void resetRefreshTokenJti(String userId, String token, Duration ttl) {
+        redisTemplate.opsForValue().setIfPresent(REFRESH_TOKEN + userId, token, ttl);
+    }
+
+    public void invalidateRefreshToken(String userId) {
+        redisTemplate.delete(REFRESH_TOKEN + userId);
     }
 }
