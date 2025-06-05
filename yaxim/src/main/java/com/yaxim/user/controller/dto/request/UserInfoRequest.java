824f7069e318b2958859a89e29c0a0e8aa6c3252
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-05 16:43:51 +0900

    refactor: OAuth로 깃 관련 정보를 자동으로 추가하는 기능이 생김에 따라 User 정보 수정 관련 기능 수정

--- yaxim/src/main/java/com/yaxim/user/controller/dto/request/UserInfoRequest.java
+++ yaxim/src/main/java/com/yaxim/user/controller/dto/request/UserInfoRequest.java
@@ -1,11 +1,12 @@
 package com.yaxim.user.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserInfoRequest {
+    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
-    private String gitEmail;
 }
