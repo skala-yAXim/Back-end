57cb54e9f413be117798bfafa193a9395db35ca8
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-12 20:54:54 +0900

    feat: 업무 데이터 통계를 위한 Repository 기능 추가

--- yaxim/src/main/java/com/yaxim/user/repository/UserRepository.java
+++ yaxim/src/main/java/com/yaxim/user/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
     boolean existsByEmail(String email);
+    List<Users> findAllByEmailIn(List<String> emails);
 }
