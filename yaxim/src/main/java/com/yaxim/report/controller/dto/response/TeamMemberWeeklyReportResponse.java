48e25fa76534f695436f277920e50ba098c94200
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-14 21:13:42 +0900

    feat: 보고서 목록 반환 DTO와 보고서 상세 반환 DTO 분리

--- yaxim/src/main/java/com/yaxim/report/controller/dto/response/TeamMemberWeeklyReportResponse.java
+++ yaxim/src/main/java/com/yaxim/report/controller/dto/response/TeamMemberWeeklyReportResponse.java
@@ -1,15 +1,15 @@
 package com.yaxim.report.controller.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
-public class ReportResponse {
+@AllArgsConstructor
+public class TeamMemberWeeklyReportResponse {
     @Schema(description = "보고서 ID")
     private Long id;
 
@@ -25,15 +25,15 @@ public class ReportResponse {
     @Schema(description = "보고서 종료일")
     private LocalDate endDate;
 
-    @Schema(description = "보고서 내용 (JSON 객체)")
-    private Object report;
+    @Schema(description = "보고서 제목")
+    private String title;
+
+    @Schema(description = "보고서 미리보기")
+    private String preview;
 
     @Schema(description = "작성자 ID (개인 보고서만 해당)")
     private Long userId;
 
     @Schema(description = "작성자 이름 (개인 보고서만 해당)")
     private String userName;
-
-    @Schema(description = "팀 ID")
-    private String teamId;
 }
