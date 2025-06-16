339c190b628ce62a278246cba80da233916be603
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-16 19:32:34 +0900

    feat: DTO 일부 수정

--- yaxim/src/main/java/com/yaxim/report/controller/dto/response/TeamMemberWeeklyReportResponse.java
+++ yaxim/src/main/java/com/yaxim/report/controller/dto/response/TeamMemberWeeklyReportResponse.java
@@ -1,11 +1,14 @@
 package com.yaxim.report.controller.dto.response;
 
+import com.yaxim.report.entity.UserWeeklyReport;
+import com.yaxim.user.entity.Users;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Getter
 @AllArgsConstructor
@@ -36,4 +39,26 @@ public class TeamMemberWeeklyReportResponse {
 
     @Schema(description = "작성자 이름 (개인 보고서만 해당)")
     private String userName;
+
+    public static TeamMemberWeeklyReportResponse fromTeam(UserWeeklyReport report) {
+        Map<String, Object> reportMap = report.getReport();
+        Users user = report.getUser();
+
+        String reportTitle = (String) reportMap.getOrDefault("report_title", "");
+        // 중첩된 Map 꺼내기
+        Map<String, Object> weeklyReportMap = (Map<String, Object>) reportMap.get("weekly_report");
+        String summary = (String) weeklyReportMap.getOrDefault("summary", "");
+
+        return new TeamMemberWeeklyReportResponse(
+                report.getId(),
+                report.getCreatedAt(),
+                report.getUpdatedAt(),
+                report.getStartDate(),
+                report.getEndDate(),
+                reportTitle,
+                summary,
+                user.getId(),
+                user.getName()
+        );
+    }
 }
