f247cb489654bf4e2f4457a770d503268f62cb6e
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-17 11:17:22 +0900

    feat: 팀 Weekly 템플릿 입력 받기 DTO및 API 추가

---
+++ yaxim/src/main/java/com/yaxim/team/controller/dto/request/WeeklyTemplateRequest.java
@@ -0,0 +1,10 @@
+package com.yaxim.team.controller.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WeeklyTemplateRequest {
+    private String template;
+}
