4bfe40425fd7f10f3df39b69a091fb526fe223f8
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-17 16:06:07 +0900

    feat: User Id로 TeamMember 찾을 수 있도록 repository 기능 추가

--- yaxim/src/main/java/com/yaxim/user/repository/UserRepository.java
+++ yaxim/src/main/java/com/yaxim/user/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package com.yaxim.user.repository;
 
-import com.yaxim.user.entity.UserRole;
 import com.yaxim.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
     boolean existsByEmail(String email);
-    List<Users> findAllByEmailIn(List<String> emails);
 }
